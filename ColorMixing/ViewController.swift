7a6679a99aa613fc417a6db10472bb787a3de65d
kirillslyusar <kirillslyusar@gmail.com>
2023-05-08 14:59:10 +0300

    new commit

--- ColorMixing/ViewController.swift
+++ ColorMixing/ViewController.swift
@@ -7,19 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var RedLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var yellowLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var greenSlader: UISlider!
+    @IBOutlet weak var yellowSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+        
+        greenLabel.text = string(from: greenSlader)
+        yellowLabel.text = string(from: yellowSlider)
+        blueLabel.text = string(from: blueSlider)
+        
     }
-
-
-    @IBAction func colorChange() {
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender {
+        case greenLabel:
+            greenLabel.text = string(from: greenSlader)
+        case yellowLabel:
+            yellowLabel.text = string(from: yellowSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            green: CGFloat(greenSlader.value),
+            yellow: CGFloat(yellowSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) ->
+    String {
+        String(format: "%.2f", slider.value)
     }
 }
 
